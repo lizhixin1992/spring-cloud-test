b2c43ce3cb08822539fffad37982eb67269cdec5
lizhixin <lzx920911@gmail.com>
2019-07-22 17:19:19 +0800

    commit

---
+++ cloud-zipkin/src/main/java/com/lzx/cloud/zipkin/CloudZipkinApplication.java
@@ -0,0 +1,52 @@
+package com.lzx.cloud.zipkin;
+
+import brave.sampler.Sampler;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@SpringBootApplication
+@RestController
+@Slf4j
+public class CloudZipkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudZipkinApplication.class, args);
+    }
+
+    private static final Logger LOG = Logger.getLogger(CloudZipkinApplication.class.getName());
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
+    @RequestMapping("/hi")
+    public String callHome(){
+        LOG.log(Level.INFO, "calling trace service-hi  ");
+        return restTemplate.getForObject("http://localhost:8087/miya", String.class);
+    }
+    @RequestMapping("/info")
+    public String info(){
+        LOG.log(Level.INFO, "calling trace service-hi ");
+
+        return "i'm service-hi";
+
+    }
+
+    @Bean
+    public Sampler defaultSampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
+
+}
