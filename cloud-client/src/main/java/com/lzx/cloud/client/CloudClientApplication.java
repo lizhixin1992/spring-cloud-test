f3ec25d5d0e905a5e311e194d3755f28f54927a9
lizhixin <lzx920911@gmail.com>
2019-07-26 15:01:26 +0800

    commit

--- cloud-client/src/main/java/com/lzx/cloud/client/CloudClientApplication.java
+++ cloud-client/src/main/java/com/lzx/cloud/client/CloudClientApplication.java
@@ -2,15 +2,11 @@ package com.lzx.cloud.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @EnableDiscoveryClient
 @EnableFeignClients
-@EnableCircuitBreaker
 @SpringBootApplication
 public class CloudClientApplication {
 
