d61a2ca197d38bcb916b74695d01c3f413334097
lizhixin <lzx920911@gmail.com>
2020-09-10 15:31:52 +0800

    测试函数式编程

--- cloud-client/src/test/java/com/lzx/cloud/client/CloudClientApplicationTests.java
+++ cloud-client/src/test/java/com/lzx/cloud/client/CloudClientApplicationTests.java
@@ -5,6 +5,9 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.function.Consumer;
+
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CloudClientApplicationTests {
@@ -13,4 +16,35 @@ public class CloudClientApplicationTests {
     public void contextLoads() {
     }
 
+    @Test
+    public void testSteam(){
+        Consumer consumer = i -> System.out.println(i);
+        consumer.accept(2);
+    }
+
+    @Test
+    public void testSteam1(){
+        Consumer consumer = new Consumer() {
+            @Override
+            public void accept(Object o) {
+                System.out.println(o);
+            }
+        };
+        consumer.accept(2);
+    }
+
+    @Test
+    public void testSteam2(){
+        Consumer consumer = i -> {
+            String s = "我是第" + i + "个；";
+            System.out.println(s);
+        };
+        pr(consumer);
+    }
+
+    public void pr(Consumer<Integer> consumer){
+        for (int i = 0; i < 10; i++) {
+             consumer.accept(i);
+        }
+    }
 }
