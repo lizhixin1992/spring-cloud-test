669c6625d593f27fb7d70f497ff294ad6dcdb5db
lizhixin <lzx920911@gmail.com>
2020-10-10 11:25:02 +0800

    测试Introspector与BeanUtils

--- cloud-client/src/test/java/com/lzx/cloud/client/CloudClientApplicationTests.java
+++ cloud-client/src/test/java/com/lzx/cloud/client/CloudClientApplicationTests.java
@@ -1,22 +1,38 @@
 package com.lzx.cloud.client;
 
+import com.alibaba.fastjson.JSONObject;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lzx.cloud.client.entity.User;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.BeanUtils;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.management.ObjectName;
+import javax.validation.constraints.NotNull;
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.time.Instant;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.UUID;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 
 @RunWith(SpringRunner.class)
@@ -125,14 +141,14 @@ public class CloudClientApplicationTests {
     }
 
     @Test
-    public void testTobData(){
+    public void testTobData() {
         Map<String, Object> map = new HashMap<>(10);
         map.put("accesskey", "lizhixinvpn");
         map.put("accessid", "VMS");
         map.put("time", Instant.now().toEpochMilli());
         map.put("channelId", "EPGC1386744804340101");
         map.put("channelName", "CCTV");
-        String sign = createSign(map,"");
+        String sign = createSign(map, "");
         map.put("sign", sign);
         String url = mapToUrlParams(map);
         System.out.println(url);
@@ -167,9 +183,9 @@ public class CloudClientApplicationTests {
      */
     public String sha1Hex(String str) {
         try {
-            if(StringUtils.isNotBlank(str)){
+            if (StringUtils.isNotBlank(str)) {
                 return DigestUtils.sha1Hex(str).toUpperCase();
-            }else {
+            } else {
                 return null;
             }
         } catch (Exception e) {
@@ -196,6 +212,100 @@ public class CloudClientApplicationTests {
             s = StringUtils.substringBeforeLast(s, "&");
         }
         return s;
+    }
+
+    @Test
+    public void TestReflect() throws Exception {
+        User user = new User();
+        user.setId(1L);
+        user.setName("test");
+        user.setAge(12);
+        user.setUsername("test");
+        user.setRequestId("12121212");
+        user.setBalance(12.0);
+        ObjectMapper objectMapper = new ObjectMapper();
+        System.out.println(objectMapper.writeValueAsString(user));
+        System.out.println("-------------------------------------");
+
+//        Class clazz = user.getClass();
+//        Field[] fields = clazz.getDeclaredFields();
+//        Arrays.stream(fields).map(Field::getName).forEach(x -> System.out.println(x));
+//        System.out.println("-------------------------------------");
+//        Method[] methods = clazz.getMethods();
+//        Arrays.stream(methods).map(Method::getName).forEach(x -> System.out.println(x));
+//        System.out.println("-------------------------------------");
+//        String value = reflectGetMethod(user, "name");
+//        System.out.println("reflectGetMethod----外------" + value);
+        reflectGetMethod1(user,"name");
+    }
+
+    /**
+     * @Description 反射获取value
+     * @Date 17:25 2020/10/9
+     **/
+    private String reflectGetMethod(User user, String fieldName) {
+        try {
+            BeanInfo beanInfo = Introspector.getBeanInfo(user.getClass());
+            List<PropertyDescriptor> descriptors = Arrays.stream(beanInfo.getPropertyDescriptors()).filter(p -> {
+                String name = p.getName();
+                //过滤掉不需要修改的属性
+                return !"class".equals(name);
+            }).collect(Collectors.toList());
+            Map<String, PropertyDescriptor> map = new LinkedHashMap<>(descriptors.size());
+            descriptors.stream().forEach(x -> map.put(x.getName(), x));
+            if(map.get(fieldName) != null){
+                PropertyDescriptor descriptor = map.get(fieldName);
+                Method readMethod = descriptor.getReadMethod();
+                Object o = readMethod.invoke(user);
+                System.out.println(descriptor.getName() + ":" + o);
+            }
+        } catch (IntrospectionException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return "12121";
+    }
+
+    /**
+     * @Description 反射获取value
+     * @Date 17:25 2020/10/9
+     **/
+    private String reflectGetMethod1(User user, String fieldName) {
+        try {
+            PropertyDescriptor descriptor = BeanUtils.getPropertyDescriptor(user.getClass(), fieldName);
+            Method readMethod = descriptor.getReadMethod();
+            Object o = readMethod.invoke(user);
+            System.out.println(descriptor.getName() + ":" + o);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return "12121";
+    }
+
+    @Test
+    public void testSwitch() {
+        Map<String, String> map = new HashMap<>();
+        map.put("1", "1");
+        map.put("2", "2");
+        map.put("3", "3");
+        map.put("4", "4");
+
+        for (String s : map.keySet()) {
+            switch (s) {
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                    System.out.println(map.get("4"));
+                    break;
+            }
+        }
+
     }
 
 }
