33e0e2414c1cac345b6ea3400d2d45cc5c30a348
lizhixin <lzx920911@gmail.com>
2021-07-27 10:21:05 +0800

    commit

--- cloud-client/src/test/java/com/lzx/cloud/client/CloudClientApplicationTests.java
+++ cloud-client/src/test/java/com/lzx/cloud/client/CloudClientApplicationTests.java
@@ -4,8 +4,11 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.parser.Feature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.protobuf.ByteString;
+import com.google.protobuf.InvalidProtocolBufferException;
 import com.lzx.cloud.client.entity.User;
 import com.lzx.cloud.client.util.HttpUtils;
+import com.lzx.cloud.protoc.PersonTestProtos;
 import com.netflix.discovery.CommonConstants;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -24,6 +27,8 @@ import java.beans.BeanInfo;
 import java.beans.IntrospectionException;
 import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -444,6 +449,67 @@ public class CloudClientApplicationTests {
         FileInputStream fileInputStream = new FileInputStream(file);
         json = JSON.parseObject(fileInputStream, Map.class, Feature.AutoCloseSource);
         System.out.println(JSON.toJSONString(json));
+
+        JSONObject.toJSONString(new HashMap<>());
+    }
+
+    @Test
+    public void testProtoc(){
+        try {
+            /** Step1：生成 personTest 对象 */
+            // personTest 构造器
+            PersonTestProtos.PersonTest.Builder personBuilder = PersonTestProtos.PersonTest.newBuilder();
+            // personTest 赋值
+            personBuilder.setName("Jet Chen");
+            personBuilder.setEmail("ckk505214992@gmail.com");
+            personBuilder.setSex(PersonTestProtos.PersonTest.Sex.MALE);
+
+            // 内部的 PhoneNumber 构造器
+            PersonTestProtos.PersonTest.PhoneNumber.Builder phoneNumberBuilder = PersonTestProtos.PersonTest.PhoneNumber.newBuilder();
+            // PhoneNumber 赋值
+            phoneNumberBuilder.setType(PersonTestProtos.PersonTest.PhoneNumber.PhoneType.MOBILE);
+            phoneNumberBuilder.setNumber("17717037257");
+
+            // personTest 设置 PhoneNumber
+            personBuilder.addPhone(phoneNumberBuilder);
+
+            // 生成 personTest 对象
+            PersonTestProtos.PersonTest personTest = personBuilder.build();
+
+            /** Step2：序列化和反序列化 */
+            // 方式一 byte[]：
+            // 序列化
+//            byte[] bytes = personTest.toByteArray();
+            // 反序列化
+//            PersonTestProtos.PersonTest personTestResult = PersonTestProtos.PersonTest.parseFrom(bytes);
+//            System.out.println(String.format("反序列化得到的信息，姓名：%s，性别：%d，手机号：%s", personTestResult.getName(), personTest.getSexValue(), personTest.getPhone(0).getNumber()));
+
+
+
+            // 方式二 ByteString：
+            // 序列化
+            ByteString byteString = personTest.toByteString();
+            System.out.println(byteString.toString());
+            System.out.println(personTest);
+            // 反序列化
+            PersonTestProtos.PersonTest personTestResult = PersonTestProtos.PersonTest.parseFrom(byteString);
+            System.out.println(String.format("反序列化得到的信息，姓名：%s，性别：%d，手机号：%s", personTestResult.getName(), personTestResult.getSexValue(), personTestResult.getPhone(0).getNumber()));
+
+
+
+            // 方式三 InputStream
+            // 粘包,将一个或者多个protobuf 对象字节写入 stream
+            // 序列化
+//            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+//            personTest.writeDelimitedTo(byteArrayOutputStream);
+            // 反序列化，从 steam 中读取一个或者多个 protobuf 字节对象
+//            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
+//            PersonTestProtos.PersonTest personTestResult = PersonTestProtos.PersonTest.parseDelimitedFrom(byteArrayInputStream);
+//            System.out.println(String.format("反序列化得到的信息，姓名：%s，性别：%d，手机号：%s", personTestResult.getName(), personTest.getSexValue(), personTest.getPhone(0).getNumber()));
+
+        } catch (InvalidProtocolBufferException e) {
+            e.printStackTrace();
+        }
     }
 
 }
