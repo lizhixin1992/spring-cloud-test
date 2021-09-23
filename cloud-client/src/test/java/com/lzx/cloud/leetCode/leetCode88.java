3375f479485f13f2cb0f7db8e8d92c3e952a326d
lizhixin <lzx920911@gmail.com>
2021-09-23 09:27:42 +0800

    leetCode

--- cloud-client/src/test/java/com/lzx/cloud/leetCode/leetCode88.java
+++ cloud-client/src/test/java/com/lzx/cloud/leetCode/leetCode88.java
@@ -1,5 +1,7 @@
 package com.lzx.cloud.leetCode;
 
+import java.util.Arrays;
+
 public class leetCode88 {
 
     public static void main(String[] args) {
@@ -7,5 +9,12 @@ public class leetCode88 {
         int m = 3;
         int[] nums2 = new int[]{2, 5, 6};
         int n = 3;
+
+        for (int i = 0; i < n; i++) {
+            nums1[m + i] = nums2[i];
+        }
+        Arrays.sort(nums1);
+
+        Arrays.stream(nums1).forEach(System.out::println);
     }
 }
