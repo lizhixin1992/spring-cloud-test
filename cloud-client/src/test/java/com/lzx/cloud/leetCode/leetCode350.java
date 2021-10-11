da6c508c250ad352aa5cd8b36a70b470cae8aef3
lizhixin <lzx920911@gmail.com>
2021-10-11 09:12:27 +0800

    commit

---
+++ cloud-client/src/test/java/com/lzx/cloud/leetCode/leetCode350.java
@@ -0,0 +1,17 @@
+package com.lzx.cloud.leetCode;
+
+public class leetCode350 {
+    public static void main(String[] args) {
+        int[] nums1 = new int[]{1, 2, 2, 1};
+        int[] nums2 = new int[]{2, 2};
+        int[] num = new int[]{};
+
+        for (int i : nums2) {
+            int a = nums2[i];
+            for (int i1 : nums1) {
+                if (a == nums1[i1]) {
+                }
+            }
+        }
+    }
+}
