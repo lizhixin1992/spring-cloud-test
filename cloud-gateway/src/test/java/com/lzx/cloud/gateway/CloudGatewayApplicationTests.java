2efa3584d15b22db60e7c54e7ffc7eb6896a1f73
lizhixin <lzx920911@gmail.com>
2021-10-26 14:31:44 +0800

    cloud-gateway init

---
+++ cloud-gateway/src/test/java/com/lzx/cloud/gateway/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lzx.cloud.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
